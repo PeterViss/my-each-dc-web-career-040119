86ba9f120de09848f831788368c78760610fe31b
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 18:06:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array) # put argument(s) here
+while my_each < 5
+  return my_each.collect do |i|
+    i
+  end
+end  # code here
+end 
